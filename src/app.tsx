4ab0f1704b71f407e9c04090ebda3d07b7a9d440
Samwel Omwenga <samwel.omwenga1@students.jkuat.ac.ke>
2026-02-08 13:25:00 +0300

    feat: add TanStack Query for state management

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,12 @@
 import { Logo } from "./components/logo"
 import { SearchInput } from "./components/search-input"
 import { UnitsConverter } from "./components/units-converter"
+import Provider from "./components/provider"
+
 
 function App() {
   return (
-    <>
-      
+    <Provider>
       <div className="flex items-center justify-between p-4">
         <Logo />
         <UnitsConverter />
@@ -51,7 +52,7 @@ function App() {
         <a href="#">Your Name Here</a>
         .
       </div>
-    </>
+    </Provider>
 
   )
 }
